b20aa612723d2261bf1f159c07997002d39b1245
eottabom <snoopy12oyk@gmail.com>
2024-06-25 16:39:58 +0900

    fix(benchmark): Add Test clazz

--- benchmark/src/main/java/lego/benchmark/finalkeyword/Test.java
+++ benchmark/src/main/java/lego/benchmark/finalkeyword/Test.java
@@ -1,2 +1,21 @@
-package lego.benchmark.finalkeyword;public class Test {
+package lego.benchmark.finalkeyword;
+
+public class Test {
+
+	public static String nonFinalStrings() {
+		String x = "x";
+		String y = "y";
+		return x + y;
+	}
+
+	public static String compilerOptimizationString() {
+		return "xy";
+	}
+
+	public static String finalStrings() {
+		final String x = "x";
+		final String y = "y";
+		return x + y;
+	}
+
 }
