e93323785628d42528b5fbc518b9fd66cd42851d
eottabom <snoopy12oyk@gmail.com>
2024-06-25 15:42:46 +0900

    Add benchmark modules - feat

---
+++ benchmark/README.md
@@ -0,0 +1,9 @@
+
+
+### final string benchmark result
+
+---
+
+BenchmarkRunner.compilerOptimizationString  avgt    5  0.339 ± 0.006  ns/op
+BenchmarkRunner.finalStrings                avgt    5  0.340 ± 0.004  ns/op
+BenchmarkRunner.nonFinalStrings             avgt    5  4.404 ± 0.188  ns/op
---
+++ benchmark/src/main/java/lego/benchmark/finalkeyword/BenchmarkRunner.java
@@ -0,0 +1,48 @@
+package lego.benchmark.finalkeyword;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+
+public class BenchmarkRunner {
+
+	public static void main(String[] args) throws IOException {
+		String[] jmhArgs = {
+				"-f", "1", // forks
+				"-wi", "5", // warmup iterations
+				"-i", "5", // Measurements iterations
+				"-t", "1", // Threads
+		};
+		org.openjdk.jmh.Main.main(jmhArgs);
+	}
+
+	@Benchmark
+	@OutputTimeUnit(TimeUnit.NANOSECONDS)
+	@BenchmarkMode(Mode.AverageTime)
+	public static String nonFinalStrings() {
+		String x = "x";
+		String y = "y";
+		return x + y;
+	}
+
+	@Benchmark
+	@OutputTimeUnit(TimeUnit.NANOSECONDS)
+	@BenchmarkMode(Mode.AverageTime)
+	public static String compilerOptimizationString() {
+		String xy = "xy";
+		return xy;
+	}
+
+	@Benchmark
+	@OutputTimeUnit(TimeUnit.NANOSECONDS)
+	@BenchmarkMode(Mode.AverageTime)
+	public static String finalStrings() {
+		final String x = "x";
+		final String y = "y";
+		return x + y;
+	}
+}
