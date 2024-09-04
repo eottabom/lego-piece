4a2d655bb1064305f60fa5aceb3b7ddc194d6da9
eottabom <snoopy12oyk@gmail.com>
2024-09-04 15:30:30 +0900

    Add

--- example/src/main/java/lego/example/lombok/User.java
+++ example/src/main/java/lego/example/lombok/User.java
@@ -15,6 +15,8 @@ import lombok.RequiredArgsConstructor;
 @NoArgsConstructor
 @RequiredArgsConstructor
 @Data
+// 명시적으로 포함될 필드를 지정한다.
+//@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
 
 	@Id
@@ -27,4 +29,30 @@ public class User {
 	@NonNull
 	private String email;
 
+	// equals() 와 hashCode() 를 직접 구현하여, id 가 null 일 때와 아닐 때를 적절히 처리한다.
+	// id 가 null 일 경우 name 과 email 필드만으로 비교하고,
+	// id 가 아닐 때 id 를 기준으로 비교한다.
+	// 이렇게 하면 영속화 전후에 equals() 와 hashCode() 가 적절히 동작한다.
+//	@Override
+//	public boolean equals(Object o) {
+//		if (this == o) {
+//			return true;
+//		}
+//		if (o == null || getClass() != o.getClass()) {
+//			return false;
+//		}
+//		User user = (User) o;
+//		if (this.id != null && user.id != null) {
+//			return this.id.equals(user.id);
+//		}
+//		return this.name.equals(user.name) && this.email.equals(user.email);
+//	}
+//
+//	@Override
+//	public int hashCode() {
+//		if (this.id != null) {
+//			return this.id.hashCode();
+//		}
+//		return this.name.hashCode() + this.email.hashCode();
+//	}
 }
